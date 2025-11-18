41b1ef396da2e1a72ffa45ace26d46b923380891
safiafares20073 <ayafares20193@gmail.com>
2025-11-18 11:50:21 +0300

    lab2 arrays

---
+++ src/arrays/Arrayss.java
@@ -0,0 +1,14 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Arrayss {
+    Scanner scanner=new Scanner(System.in);
+    public static void traversal2(int[]a) {
+        for(int e:a)
+            System.out.println(e);    }
+
+    public static void input(int a[]) {
+        System.out.println();
+    }
+}
---
+++ src/arrays/Main.java
@@ -0,0 +1,13 @@
+package arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //int []numbers=new int[5];
+         Arrayss arr2 = new Arrayss();
+         int[]numbers={1,2,3,4,5};
+         numbers[0]=10;
+         arr2.traversal2(numbers);
+    }
+}
